3f09860618b385cb8ab474343e847fe32f3cd096
Ciunas Bennett <ciunas.bennett@gmail.com>
2017-12-17 20:43:10 +0000

    Progress on the layout, tree view created. Select element in tree.

--- src/notepad/NotePad.java
+++ src/notepad/NotePad.java
@@ -1,7 +1,6 @@
 package notepad;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
@@ -10,13 +9,41 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.border.BevelBorder;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeSelectionModel;
+import java.awt.GridLayout;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.prefs.Preferences;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+import javax.swing.JTree;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+/**
+ * @author ciunas
+ *
+ */
 public class NotePad {
 
 	private JFrame frame;
+	private JLabel lblNewLabel;
+	private JPanel panel_4;
+	private static DefaultMutableTreeNode root;
+	private static DefaultTreeModel treeModel;
+	private JTree tree;
+	private Preferences prefs;
 
 	/**
 	 * Launch the application.
@@ -24,21 +51,22 @@ public class NotePad {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				// getResourceAsFile();
 				try {
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
-					UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");					
-					//fUIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");					
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");				
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.fast.FastLookAndFeel");					
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");			
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.mint.MintLookAndFeel");
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.luna.LunaLookAndFeel");
-					//UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
- 				
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
+					UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");
+					// fUIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.fast.FastLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.mint.MintLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.luna.LunaLookAndFeel");
+					// UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
 				} catch (InstantiationException e) {
@@ -69,32 +97,137 @@ public class NotePad {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
+		String rootDir = setGetPreference("Root", "", "get");
+		File fileRoot = new File(rootDir);
+
+		root = new DefaultMutableTreeNode(new FileNode(fileRoot));
+		treeModel = new DefaultTreeModel(root);
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 818, 719);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1, BorderLayout.SOUTH);
 		
+		JLabel lblNewLabel_1 = new JLabel("Made By: Ciunas Bennet");
+		panel_1.add(lblNewLabel_1);
+
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2, BorderLayout.CENTER);
 		panel_2.setLayout(new BorderLayout(0, 0));
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		panel_2.add(tabbedPane, BorderLayout.CENTER);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		tabbedPane.addTab("New tab", null, scrollPane, null);
-		
+		tabbedPane.addTab("Blank", null, scrollPane, null);
+
 		JTextArea textArea = new JTextArea();
 		textArea.setForeground(Color.GREEN);
 		textArea.setBackground(Color.BLACK);
-		textArea.setFont(new Font("Dialog", Font.PLAIN, 13));
+		textArea.setFont(new Font("Cantarell", Font.PLAIN, 13));
 		scrollPane.setViewportView(textArea);
+
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.LIGHT_GRAY);
+		panel_3.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		panel.add(panel_3, BorderLayout.WEST);
+		panel_3.setLayout(new BorderLayout(0, 0));
+
+		lblNewLabel = new JLabel("Tree");
+		lblNewLabel.setPreferredSize(new Dimension(50, 25));
+		panel_3.add(lblNewLabel, BorderLayout.NORTH);
+
+		panel_4 = new JPanel();
+		panel_3.add(panel_4, BorderLayout.CENTER);
+		panel_4.setLayout(new MigLayout("", "[grow]", "[grow][grow][grow]"));
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		panel_4.add(scrollPane_1, "cell 0 0 1 2,grow");
+		tree = new JTree(treeModel);
+		tree.setShowsRootHandles(true);
+		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		
+				tree.addTreeSelectionListener(new TreeSelectionListener() {
+					@Override
+					public void valueChanged(TreeSelectionEvent e) {
+						DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+						Object userObject = selectedNode.getUserObject();
+						System.out.println(rootDir + userObject);
+					}
+				});
+
+		JPanel panel_5 = new JPanel();
+		scrollPane_1.setViewportView(panel_5);
+		panel_5.setLayout(new BorderLayout(0, 0));
+
+		JButton btnNewButton = new JButton("Set Root");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				changeRoot();
+			}
+		});
+		panel_5.add(btnNewButton, BorderLayout.SOUTH);
+
+		panel_5.add(tree, BorderLayout.CENTER);
+
+		JPanel panel_6 = new JPanel();
+		panel_4.add(panel_6, "cell 0 2,grow");
+		panel_6.setLayout(new GridLayout(2, 1, 0, 0));
+
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_6.add(btnNewButton_1);
+
+		JButton button = new JButton("New button");
+		panel_6.add(button);
+
+		getList(root, fileRoot);
 	}
 
+	public void changeRoot() {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFileChooser f = new JFileChooser();
+				f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				f.showSaveDialog(null);
+				setGetPreference("Root", f.getSelectedFile().toString(), "set");
+			}
+		});
+	}
+
+	public void getList(DefaultMutableTreeNode node, File f) {
+
+		File[] files = f.listFiles();
+		if (files == null)
+			return;
+
+		for (File file : files) {
+			DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(new FileNode(file));
+			node.add(childNode);
+
+			if (file.isHidden()) {
+
+			} else if (file.isDirectory()) {
+				getList(childNode, file);
+			}
+		}
+	}
+
+	public String setGetPreference(String id, String value, String SetGet) {
+		// This will define a node in which the preferences can be stored
+		prefs = Preferences.userRoot().node(this.getClass().getName());
+
+		if (SetGet.equals("get")) {
+			return (prefs.get(id, ""));
+		} else
+			prefs.put(id, value);
+		return "";
+	}
 }
